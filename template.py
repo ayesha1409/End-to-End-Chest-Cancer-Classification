c8e735f142a15ade9cd73b12ce0180e72a330dc6
AyeshaArshad_MCA_A_2024_26_15_UEMK <105972962+ayesha1409@users.noreply.github.com>
2025-09-28 21:00:02 +0530

    Update template.py

--- template.py
+++ template.py
@@ -1,1 +1,36 @@
+import os
+
+# list of files you want to create
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    "data/.gitkeep",
+    "notebooks/.gitkeep",
+    "src/cnnClassifier/__init__.py",
+    "src/cnnClassifier/utils/__init__.py",
+    "src/cnnClassifier/utils/logger.py",
+    "src/cnnClassifier/utils/common.py",
+    "src/cnnClassifier/components/__init__.py",
+    "src/cnnClassifier/config/__init__.py",
+    "src/cnnClassifier/pipeline/__init__.py",
+    "src/cnnClassifier/entity/__init__.py",
+    "src/cnnClassifier/constants/__init__.py",
+    "tests/__init__.py",
+    "tests/unit/__init__.py",
+    "tests/integration/__init__.py",
+    "init_setup.sh",
+    "requirements.txt",
+    "setup.py",
+    "main.py",
+    "README.md",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
 
